8bafd45a19ed0908d4f7552e37dbabb9d7c78f6a
giomungcal <48665546+giomungcal@users.noreply.github.com>
2020-03-31 22:19:52 -0600

    Updates

---
+++ precache-manifest.1d3a2af2cbbbafeb55992ca5a24b0fb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f89a9d9fd53b6ba6b94c8fccefd598c",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "9b767c74cc6fa310d338",
+    "url": "/my-portfolio/static/css/main.dc8020e0.chunk.css"
+  },
+  {
+    "revision": "a197a7ad2c165b3a4dd0",
+    "url": "/my-portfolio/static/js/2.28d6f8c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-portfolio/static/js/2.28d6f8c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b767c74cc6fa310d338",
+    "url": "/my-portfolio/static/js/main.5c07b820.chunk.js"
+  },
+  {
+    "revision": "6d7a7337562760299d40",
+    "url": "/my-portfolio/static/js/runtime-main.00bba678.js"
+  },
+  {
+    "revision": "efe36cb1e690638fc59af446ffc5e774",
+    "url": "/my-portfolio/static/media/BebasNeue-Regular.efe36cb1.otf"
+  },
+  {
+    "revision": "d8615485c60d912f390ae3b7d33420d8",
+    "url": "/my-portfolio/static/media/KayakSansRegular.d8615485.otf"
+  }
+]);
